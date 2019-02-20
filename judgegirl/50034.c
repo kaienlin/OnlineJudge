384d9a9190bcaf2c014950b6c860c627bb0d7a92
kaienlin <kaienlin1999@gmail.com>
2019-02-20 23:16:36 +0800

    complete judgegirl 50034 - all C programming problems done

--- judgegirl/50034.c
+++ judgegirl/50034.c
@@ -4,27 +4,41 @@
  
 int n, seesaw[18], w[18], used[18];
 int mtot, b_point, done;
+int *sorted[18];
+int map[18];
+int lookup[18][18];
 
-int cmp(const void *a, const void *b) {
-    return *(int*)a - *(int*)b;
+int possible(int idx, int rm) {
+	int max = rm, min = rm;
+	for ( int i = 0, c = 0; i < n; i++)
+		if ( !used[map[i]] )
+			max += lookup[map[i]][idx + (c++)];
+	for ( int i = n - 1, c = 0; i >= 0; i--)
+		if ( !used[map[i]] )
+			min += lookup[map[i]][idx + (c++)];
+	return !( max < b_point * mtot || min > b_point * mtot );
 }
- 
-void rec(int idx, int rm, int mt) {
-    if ( done || rm > b_point * mt ) {return;}
+
+void rec(int idx, int rm) {
+    if ( !possible(idx, rm) ) { return; }
     if ( idx == n ) {
         if ( rm % mtot == 0 && rm / mtot == b_point )
             done = 1;
         return;
     }
-    for ( int i = 0; i < n; i++) {
+    for ( int i = n - 1; i >= 0; i--) {
         if ( !used[i] ) {
             seesaw[idx] = w[i];
             used[i] = 1;
-            rec(idx+1, rm + idx * w[i], mt + w[i]);
+            rec(idx+1, rm + lookup[i][idx]);
             if ( done ) {return;}
             used[i] = 0;
         }
     }
+}
+
+int cmp(const void *a, const void *b) {
+	return **(int**)a > **(int**)b;
 }
  
 int main()
@@ -34,11 +48,17 @@ int main()
         for ( int i = 0; i < n; i++) {
             scanf("%d", &w[i]);
             mtot += w[i];
+			sorted[i] = &w[i];
+			for ( int j = 0; j < n; j++)
+				lookup[i][j] = j * w[i];
         }
+		qsort(sorted, n, sizeof(int*), cmp);
+		for ( int i = 0; i < n; i++)
+			map[i] = sorted[i] - w;
         b_point = (n - 1) / 2;
         done = 0;
         memset(used, 0, sizeof(used));
-        rec(0, 0, 0);
+        rec(0, 0);
         if ( done ) {
             for ( int i = 0; i < n; i++) {
                 if ( i == b_point )
